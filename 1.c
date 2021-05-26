c23dcc827fcf21bf6bdca78819d13952f418ad76
MaxSprog <maxxpankov@gmail.com>
2021-05-26 19:15:51 +0700

    1

---
+++ 1.c
@@ -0,0 +1,29 @@
+#include "bintree.h"
+
+struct node* delete_min(struct node* root){
+    if(!root) return NULL;
+    struct node* buf;
+    if(root->lt == NULL){
+        buf = root->rt;
+        free(root);
+    }
+    else{
+        buf = root;
+        buf->lt = delete_min(root->lt);
+    }
+    return buf;
+}
+
+int main(){
+    struct node *root = NULL;
+    root = add(root, 5);
+    root = add(root, 3);
+    root = add(root, 10);
+    print_tree(root);
+    printf("\n");
+    root = delete_min(root);
+    print_tree(root);
+    printf("\n");
+    root = delete_min(root);
+    print_tree(root);
+}
